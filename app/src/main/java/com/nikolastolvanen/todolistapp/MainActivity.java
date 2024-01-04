b8bfa79ebbea2bd534091d7604af603f6367d608
Nikolas Tolvanen <nikolas.tolvanen@gmail.com>
2024-01-04 21:22:42 +0200

    Add toolbar

--- app/src/main/java/com/nikolastolvanen/todolistapp/MainActivity.java
+++ app/src/main/java/com/nikolastolvanen/todolistapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.nikolastolvanen.todolistapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
 }
